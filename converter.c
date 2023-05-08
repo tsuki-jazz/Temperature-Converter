e3daadcd762754124e3fa1b15bb1448e0d0e8aef
margaret <117048397+tsuki-jazz@users.noreply.github.com>
2023-05-08 08:19:01 -0400

    Added Fahrenheit to Kelvin Conversion

--- converter.c
+++ converter.c
@@ -8,14 +8,15 @@
 
 int main(void) {
 
-	int option = 0;
+  int option = 0;
   double fahrenheit = 0, celsius = 0;
 
   {
-	printf("Welcome to the Temperature Converter!\n");
+        printf("Welcome to the Temperature Converter!\n");
 	printf("Here are the following converting options...\n");
 	printf("\t1. Fahrenheit to Celsius\n");
 	printf("\t2. Celsius to Fahrenheit\n");
+	printf("\t3. Fahrenheit to Kelvin\n");
 	printf("Please enter the number that corresponds to the options above: ");
 	scanf("%d", &option);
   }
@@ -25,7 +26,7 @@ int main(void) {
   {
 	printf("Please enter your number in Fahrenheit: ");
 	scanf("%lf", &fahrenheit);
-	printf("Your number in celsius is: %.1lf\n", ((fahrenheit - 32) * 5 / 9));
+	printf("Your number in celsius is: %.1lf\n", ((fahrenheit - 32) * 5/9));
   }
 
   else if (option == 2)
@@ -33,6 +34,13 @@ int main(void) {
 	printf("Please enter your number in Celsius: ");
 	scanf("%lf", &celsius);
 	printf("Your number in fahrenheit is: %.1lf\n", (celsius * 1.8) + 32);
+  }
+
+  else if (option == 3)
+  {
+	  printf("Please enter your number in Fahrenheit: ");
+	  scanf("%lf", &fahrenheit);
+	  printf("Your number in fahrenheit is: %.1lf\n", ((fahrenheit - 32) * 5/9 + 273.15);
   }
 
   else 
