a0a2569fcb1f23f74de5124f83474acc1c332c45
margaret <117048397+tsuki-jazz@users.noreply.github.com>
2023-05-07 22:20:05 -0400

    Add files via upload

---
+++ converter.c
@@ -0,0 +1,45 @@
+/* Temperature Converter
+ *  converter.c
+ *  Margaret A.
+ */ 
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	int option = 0;
+  double fahrenheit = 0, celsius = 0;
+
+  {
+	  printf("Welcome to the Temperature Converter!\n");
+	  printf("Here are the following converting options...\n");
+	  printf("\t1. Fahrenheit to Celsius\n");
+	  printf("\t2. Celsius to Fahrenheit\n");
+	  printf("Please enter the number that corresponds to the options above: ");
+	  scanf("%d", &option);
+  }
+  
+
+  if (option == 1)
+  {
+	  printf("Please enter your number in Fahrenheit: ");
+	  scanf("%lf", &fahrenheit);
+	  printf("Your number in celsius is: %.1lf\n", ((fahrenheit - 32) * 5 / 9));
+  }
+
+  else if (option == 2)
+  {
+	  printf("Please enter your number in Celsius: ");
+	  scanf("%lf", &celsius);
+	  printf("Your number in fahrenheit is: %.1lf\n", (celsius * 1.8) + 32);
+  }
+
+  else 
+  {
+	  printf("ERROR: Invalid option selected.");
+  }
+
+  return 0;
+	
+ }
